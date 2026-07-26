5d88a9dcba8d2d073d3faf79b3d6dbffe96ad2f9
neiljamestony <nvalerio747@gmail.com>
2026-07-26 12:34:37 +0800

    updated callback url on google

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -34,7 +34,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: "/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = {
